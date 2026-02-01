a0156dd9c398bfd035fa77db69ceacdf8436ff08
shubhamkumar9199 <shubham.24bcs10320@sst.scaler.com>
2026-02-01 17:31:45 +0000

    Deploying to gh-pages from @ shubhamkumar9199/web-app@a1ee60c77e1a2d10b239604afe75d93d5cf83448 🚀

--- browser/assets/env.js
+++ browser/assets/env.js
@@ -21,6 +21,7 @@
   window["env"]["fineractPlatformTenantIds"]  = '';
 
   window['env']['tenantLogoUrl'] = '';
+  window['env']['tenantLogoUrlDark'] = '';
 
   // Language Environment variables
   window["env"]["defaultLanguage"] = '';
@@ -58,8 +59,23 @@
   // OAuth Server URL
   window['env']['oauthServerUrl'] = '';
 
+  // OAuth Server Logout URL
+  window['env']['oauthServerLogoutUrl'] = '';
+
   // OAuth Client Id
   window['env']['oauthAppId'] = '';
+
+  // OAuth Authorize URL
+  window['env']['oauthAuthorizeUrl'] = '';
+
+  // OAuth Token URL
+  window['env']['oauthTokenUrl'] = '';
+
+  // OAuth Redirect URI
+  window['env']['oauthRedirectUri'] = '';
+
+  // OAuth Scope
+  window['env']['oauthScope'] = '';
 
 
   // Hide client data (mask names)
