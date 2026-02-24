ee2020aba26db67942542c8fb5d962dcf9750021
shubhamkumar9199 <shubham.24bcs10320@sst.scaler.com>
2026-02-24 16:54:57 +0000

    Deploying to gh-pages from @ shubhamkumar9199/web-app@162c7bae284db738e125d5b55a2153a1d8747082 🚀

--- assets/env.js
+++ assets/env.js
@@ -91,12 +91,12 @@
   window['env']['mifosInterbankTransfersEnabled'] = 'true';
 
   // Remittance Module Environment variables
-  window['env']['mifosRemittanceApiUrl'] = '';
-  window['env']['mifosRemittanceApiProvider'] = '/remittance-api';
-  window['env']['mifosRemittanceApiVersion'] = '/v1';
+  window['env']['mifosRemittanceApiClientUrl'] = '';
+  window['env']['mifosRemittanceApiProvider'] = '';
+  window['env']['mifosRemittanceApiVersion'] = '';
   window['env']['mifosRemittanceEnabled'] = false;
-  window['env']['mifosRemittanceApiHeader'] = 'X-Gravitee-Api-Key';
-  window['env']['mifosRemittanceApiKey'] = '';
+  window['env']['mifosRemittanceApiClientHeader'] = '';
+  window['env']['mifosRemittanceApiClientKey'] = '';
 
   // Enable Role-Based Access Control (RBAC) for menu/button permissions
   // Set to true to enable RBAC, false (default) for backward compatibility
